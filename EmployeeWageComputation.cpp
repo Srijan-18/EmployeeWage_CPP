6b9d2172b31c343bc6167c7d08ecc67f1751be60
Srijan-18 <srijan.mishrasir@gmail.com>
2020-08-20 02:46:05 +0530

    ADDED: Code to comply with condition of max working hours along with max working days.

--- EmployeeWageComputation.cpp
+++ EmployeeWageComputation.cpp
@@ -7,7 +7,8 @@ const int IS_PRESENT_PART_TIME = 2;
 const int HOURS_IN_FULL_DAY = 8;
 const int HOURS_IN_PART_TIME_DAY = 4;
 const int WAGE_PER_HOUR = 20;
-const int WORKING_DAYS_IN_A_MONTH = 20;
+const int MAX_WORKING_DAYS_IN_A_MONTH = 20;
+const int MAX_WORKING_HOURS_IN_MONTH = 100;
 
 int getWorkingHours();
 
@@ -15,13 +16,16 @@ int main()
 {
     cout << "WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM" << endl;
     srand(time(0));
-    int monthlyWage = 0;
-    for (int dayCounter = 1; dayCounter <= WORKING_DAYS_IN_A_MONTH; dayCounter++)
+    int monthlyWage = 0, workingDays = 0, workingHours = 0;
+    while (workingDays < MAX_WORKING_DAYS_IN_A_MONTH && workingHours < MAX_WORKING_HOURS_IN_MONTH)
     {
-        int employeeWageForTheDay = WAGE_PER_HOUR * getWorkingHours();
-        monthlyWage += employeeWageForTheDay;
+        workingHours += getWorkingHours();
+        workingDays++;
     }
-    cout << "WAGE FOR THE MONTH = " << monthlyWage << endl;
+    cout << "Working Days --> " << workingDays << endl;
+    cout << "Working Hours --> " << workingHours << endl;
+    monthlyWage = workingHours * WAGE_PER_HOUR;
+    cout << "WAGE FOR THE MONTH --> " << monthlyWage << endl;
 
     return 0;
 }
