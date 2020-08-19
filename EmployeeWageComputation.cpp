9b5d597b94eff4b7decfa009050b6ec86df4e5f8
Srijan-18 <srijan.mishrasir@gmail.com>
2020-08-20 02:32:39 +0530

    ADDED: Code to compute monthly wage of an employee on basis of max working days in a month.

--- EmployeeWageComputation.cpp
+++ EmployeeWageComputation.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <ctime>
-
 using namespace std;
 
 const int IS_PRESENT_FULL_TIME = 1;
@@ -8,33 +7,35 @@ const int IS_PRESENT_PART_TIME = 2;
 const int HOURS_IN_FULL_DAY = 8;
 const int HOURS_IN_PART_TIME_DAY = 4;
 const int WAGE_PER_HOUR = 20;
+const int WORKING_DAYS_IN_A_MONTH = 20;
 
 int getWorkingHours();
 
 int main()
 {
     cout << "WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM" << endl;
-
-    int employeeWageForTheDay = WAGE_PER_HOUR * getWorkingHours();
-    cout << "Wage for the day = " << employeeWageForTheDay << endl;
+    srand(time(0));
+    int monthlyWage = 0;
+    for (int dayCounter = 1; dayCounter <= WORKING_DAYS_IN_A_MONTH; dayCounter++)
+    {
+        int employeeWageForTheDay = WAGE_PER_HOUR * getWorkingHours();
+        monthlyWage += employeeWageForTheDay;
+    }
+    cout << "WAGE FOR THE MONTH = " << monthlyWage << endl;
 
     return 0;
 }
 
 int getWorkingHours()
 {
-    srand(time(0));
     int employeeAttendance = rand() % 3;
     switch (employeeAttendance)
     {
     case IS_PRESENT_FULL_TIME:
-        cout << "Employee is Present Full Time" << endl;
         return HOURS_IN_FULL_DAY;
     case IS_PRESENT_PART_TIME:
-        cout << "Employee is Present Part Time" << endl;
         return HOURS_IN_PART_TIME_DAY;
     default:
-        cout << "Employee is Absent" << endl;
         return 0;
     }
 }
