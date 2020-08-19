4fc88274ca99e1de42ebd0fe9be5960c47d5e907
Srijan-18 <srijan.mishrasir@gmail.com>
2020-08-20 01:22:06 +0530

    ADDED: Code to get employee attendance using random number.

--- EmployeeWageComputation.cpp
+++ EmployeeWageComputation.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include <ctime>
 
 using namespace std;
 
 int main()
 {
-    cout << "WELCOME TO EMPLOYEE WAGE COMPUTION PROGRAM" << endl;
+    srand(time(0)); 
+    const int IS_PRESENT = 1;
+    cout << "WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM" << endl;
+    int employeeAttendance = rand() % 2;
+    employeeAttendance == IS_PRESENT ? cout << "Employee Is Present" << endl : cout << "Employee Is Absent"; 
 }
