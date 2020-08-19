d3ce17cd1cccbed0a31362b18c62f78fb3afc115
Srijan-18 <srijan.mishrasir@gmail.com>
2020-08-20 01:41:53 +0530

    ADDED: Code to compute wage of an employee for a day.

--- EmployeeWageComputation.cpp
+++ EmployeeWageComputation.cpp
@@ -3,11 +3,20 @@
 
 using namespace std;
 
+const int IS_PRESENT = 1;
+const int WAGE_PER_HOUR = 20;
+const int HOURS_PER_FULL_DAY = 8;
+
 int main()
 {
-    srand(time(0)); 
-    const int IS_PRESENT = 1;
+    srand(time(0));
     cout << "WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM" << endl;
+    
     int employeeAttendance = rand() % 2;
-    employeeAttendance == IS_PRESENT ? cout << "Employee Is Present" << endl : cout << "Employee Is Absent"; 
+    employeeAttendance == IS_PRESENT ? cout << "Employee Is Present" << endl : cout << "Employee Is Absent" << endl;
+    
+    int employeeWageForTheDay = WAGE_PER_HOUR * HOURS_PER_FULL_DAY * employeeAttendance;
+    cout << "Wage for the day = " << employeeWageForTheDay << endl;
+
+    return 0;
 }
