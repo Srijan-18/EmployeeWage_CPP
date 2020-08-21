6779fa16bb622314472aed63f35df48a6dbc74a1
Srijan-18 <srijan.mishrasir@gmail.com>
2020-08-21 06:04:55 +0530

    ADDED: Ability to store wages for multiple months.

--- EmployeeWageComputation.cpp
+++ EmployeeWageComputation.cpp
@@ -12,17 +12,19 @@ class Employee {
     int hoursWorkedToday;
     vector<int> dailyWage; 
     int monthlyWage;
-    int totalWorkingDays;
-    int totalWorkingHours;
+    int totalWorkedDaysInAMonth;
+    int totalWorkedHoursInAMonth;
+    int numOfMonths;
    
     Employee(){}
     Employee(int maxNumOfDays)
     {
         this -> hoursWorkedToday = 0;
         this -> monthlyWage = 0;
-        this -> totalWorkingDays = 0;
-        this -> totalWorkingHours = 0;
+        this -> totalWorkedDaysInAMonth = 0;
+        this -> totalWorkedHoursInAMonth = 0;
         this -> dailyWage = vector<int>(maxNumOfDays);
+        this -> numOfMonths = 0;
     }
 };
 
@@ -35,49 +37,41 @@ const int MAX_WORKING_DAYS_IN_A_MONTH = 20;
 const int MAX_WORKING_HOURS_IN_MONTH = 100;
 
 int getWorkingHours();
-void writeInFile(string employeeName, string dayCount, string hoursWorkedInDay, string wageForDay, string totalWage);
+void writeInFile(string employeeName, string monthCount, string dayCount, string hoursWorkedInDay, string wageForDay, string totalWage);
 void getEmployeeNames(Employee employees[], int numOfEmployees);
+int getNumberOfMonths();
+Employee getUpdatedEmployeeInformation(int daysWorkedInPreviousMonth, Employee currentEmployee);
 
 int main()
 {
     cout << "WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM" << endl;
     srand(time(0));
- //   int monthlyWage = 0, workingDays = 0, workingHours = 0;
     remove("Data.csv");
-    writeInFile("EMPLOYEE_NAME","DAY", "HOURS_FOR_DAY", "WAGE_FOR_DAY", "TOTAL_WAGE");
+    writeInFile("EMPLOYEE_NAME", "MONTH", "DAY", "HOURS_FOR_DAY", "WAGE_FOR_DAY", "CURRENT_MONTH_WAGE");
+    const int MAX_NUM_OF_MONTHS = getNumberOfMonths();
     int numOfEmployees;
     cout << "Enter the number of employees: ";
     cin >> numOfEmployees;
     
     Employee *employees = new Employee[numOfEmployees];
         for (int employeeCount = 0; employeeCount < numOfEmployees; employeeCount++)
-        {
-            employees[employeeCount] = Employee(MAX_WORKING_DAYS_IN_A_MONTH);
-        }
+            employees[employeeCount] = Employee(MAX_WORKING_DAYS_IN_A_MONTH * MAX_NUM_OF_MONTHS);
+    
     getEmployeeNames(employees, numOfEmployees);
+    
     for (int employeeCount = 0; employeeCount < numOfEmployees; employeeCount++)
     {
-        Employee currentEmployee = Employee(MAX_WORKING_DAYS_IN_A_MONTH);
+        Employee currentEmployee = Employee(MAX_WORKING_DAYS_IN_A_MONTH * MAX_NUM_OF_MONTHS);
         currentEmployee.employeeName = employees[employeeCount].employeeName;
-        //employees[employeeCount];
-        while (currentEmployee.totalWorkingDays < MAX_WORKING_DAYS_IN_A_MONTH && currentEmployee.totalWorkingHours < MAX_WORKING_HOURS_IN_MONTH)
+        while (currentEmployee.numOfMonths < MAX_NUM_OF_MONTHS)
         {
-            currentEmployee.hoursWorkedToday = getWorkingHours();
-    //        cout << employees[employeeCount].hoursWorkedToday << "--" ;
-            currentEmployee.dailyWage.at(currentEmployee.totalWorkingDays) = currentEmployee.hoursWorkedToday * WAGE_PER_HOUR;
-  //          cout << employees[employeeCount].dailyWage.at(employees[employeeCount].totalWorkingDays) << "--";
-            currentEmployee.totalWorkingHours += employees[employeeCount].hoursWorkedToday;
-//            cout << employees[employeeCount].totalWorkingHours << "--";
-            currentEmployee.monthlyWage += currentEmployee.dailyWage[(currentEmployee.totalWorkingDays)];
-            cout << currentEmployee.dailyWage.at((currentEmployee.totalWorkingDays)) << endl;
-            writeInFile(currentEmployee.employeeName, to_string(currentEmployee.totalWorkingDays + 1), to_string(currentEmployee.hoursWorkedToday), to_string(currentEmployee.dailyWage[currentEmployee.totalWorkingDays]), to_string(currentEmployee.monthlyWage));
-           (currentEmployee.totalWorkingDays)++;
+            int daysWorkedInPreviousMonth = currentEmployee.totalWorkedDaysInAMonth;
+            currentEmployee.totalWorkedDaysInAMonth = 0;
+            currentEmployee.monthlyWage = 0;
+            currentEmployee.totalWorkedHoursInAMonth = 0;
+            currentEmployee = getUpdatedEmployeeInformation(daysWorkedInPreviousMonth, currentEmployee);
         }
-        employees[employeeCount] = currentEmployee;
     }
-    // cout << "Working Days --> " << workingDays << endl;
-    // cout << "Working Hours --> " << workingHours << endl;
-    // cout << "WAGE FOR THE MONTH --> " << monthlyWage << endl;
 
     return 0;
 }
@@ -99,18 +93,41 @@ int getWorkingHours()
 void getEmployeeNames(Employee employees[], int numOfEmployees) {
     for(int employeeCount = 0; employeeCount < numOfEmployees; employeeCount++)
     {
-        cout << "Enter the Name of Emloyee Number " << employeeCount + 1 << " : ";
+        cout << "Enter the Name of Employee Number " << employeeCount + 1 << " : ";
         cin >> employees[employeeCount].employeeName;
     }
 }
 
-void writeInFile(string employeeName, string dayCount, string hoursWorkedInDay, string wageForDay, string totalWage)
+int getNumberOfMonths(){
+    int months;
+    cout << "Enter Number Of Months To Calculate Wage For: ";
+    cin >> months;
+    return months;
+}
+
+Employee getUpdatedEmployeeInformation(int daysWorkedInPreviousMonth, Employee currentEmployee) 
+{
+
+    while (currentEmployee.totalWorkedDaysInAMonth < MAX_WORKING_DAYS_IN_A_MONTH && currentEmployee.totalWorkedHoursInAMonth < MAX_WORKING_HOURS_IN_MONTH)
+            {
+                currentEmployee.hoursWorkedToday = getWorkingHours();
+                currentEmployee.dailyWage.at(daysWorkedInPreviousMonth + currentEmployee.totalWorkedDaysInAMonth) = currentEmployee.hoursWorkedToday * WAGE_PER_HOUR;
+                currentEmployee.totalWorkedHoursInAMonth += currentEmployee.hoursWorkedToday;
+                currentEmployee.monthlyWage += currentEmployee.dailyWage[(currentEmployee.totalWorkedDaysInAMonth)];
+                writeInFile(currentEmployee.employeeName, to_string(currentEmployee.numOfMonths + 1), to_string(currentEmployee.totalWorkedDaysInAMonth + 1), to_string(currentEmployee.hoursWorkedToday), to_string(currentEmployee.dailyWage[daysWorkedInPreviousMonth + currentEmployee.totalWorkedDaysInAMonth]), to_string(currentEmployee.monthlyWage));
+            (currentEmployee.totalWorkedDaysInAMonth)++;
+            }
+            currentEmployee.numOfMonths++;
+            return currentEmployee;
+}
+void writeInFile(string employeeName, string monthCount, string dayCount, string hoursWorkedInDay, string wageForDay, string totalWage)
 {
     fstream fileStream;
     fileStream.open("Data.csv", ios::out | ios::app);
     if (fileStream.is_open())
     {
         fileStream << employeeName << ",";
+        fileStream << monthCount << ",";
         fileStream << dayCount << ",";
         fileStream << hoursWorkedInDay << ",";
         fileStream << wageForDay << ",";
